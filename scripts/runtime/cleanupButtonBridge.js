df9721a5532809f9f618d3d9e7a6cc215bbe6ab8
steep-47 <2157528478@qq.com>
2026-09-01 02:41:30 +0800

    fix: invalidate cleanup runtime cache

--- scripts/runtime/cleanupButtonBridge.js
+++ scripts/runtime/cleanupButtonBridge.js
@@ -1,4 +1,4 @@
-import { runStableCleanup } from './stableTableCleanup.js?v=memon6';
+import { runStableCleanup } from './stableTableCleanup.js?v=memon86';
 
 const INSTALL_FLAG = '__memoCleanupButtonBridgeInstalled';
 
