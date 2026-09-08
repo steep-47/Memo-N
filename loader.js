e6f848d32f152423969ebb236796b36cbbc4a184
steep-47 <2157528478@qq.com>
2026-09-08 14:15:06 +0800

    chore: load cleanup stream heartbeat runtime

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
-const RUNTIME_VERSION = '0.26-appearance-light-refine';
-const DISPLAY_VERSION = '0.26';
+const RUNTIME_VERSION = '0.27-cleanup-stream-heartbeat';
+const DISPLAY_VERSION = '0.27';
 
 // index.js 必须保持唯一的标准模块URL。
 // core/manager.js 会循环引用 ../index.js；若这里给 index.js 加 ?v= 查询参数，浏览器会把两者当成两个模块，
@@ -66,4 +66,4 @@ jQuery(() => {
     $('#tableUpdateTag').show().text(`v${DISPLAY_VERSION}`);
 });
 
-console.log('[Memo-N][loader] v0.26 appearance light refinement runtime loaded');
+console.log('[Memo-N][loader] v0.27 cleanup stream heartbeat runtime loaded');
